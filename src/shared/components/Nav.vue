842c31ae781140728e6da8e05273b1490c6b7e10
rodrigues@novusti.com <thiagonr86@gmail.com>
2021-03-18 20:10:09 -0300

    app navbar

---
+++ src/shared/components/Nav.vue
@@ -0,0 +1,12 @@
+<template>
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+        <b-navbar-brand href="#">Guessing App</b-navbar-brand>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-collapse id="nav-collapse" is-nav>
+            <b-navbar-nav>
+                <b-nav-item href="#">Guessing</b-nav-item>
+                <b-nav-item href="#">Results</b-nav-item>
+            </b-navbar-nav>
+        </b-collapse>
+    </b-navbar>
+</template>
