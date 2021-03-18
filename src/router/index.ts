b3d9d2a8b60c8336e998676e6005080cbf88a4e1
rodrigues@novusti.com <thiagonr86@gmail.com>
2021-03-18 20:10:52 -0300

    guessing initial route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
+import Guessing from '@/views/Guessing.vue'
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Guessing',
+    component: Guessing
   },
   {
     path: '/about',
