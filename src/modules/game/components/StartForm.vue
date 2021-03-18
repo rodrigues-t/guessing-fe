ab0d12d77207f6b388608c090033da9142a2988a
rodrigues@novusti.com <thiagonr86@gmail.com>
2021-03-18 20:11:11 -0300

    start game form

---
+++ src/modules/game/components/StartForm.vue
@@ -0,0 +1,116 @@
+<template>
+  <b-card border-variant="dark">
+    <ValidationObserver ref="formObserver">
+      <b-form>
+        <b-form-group
+          id="input-group-1"
+          label="Movie search term"
+          label-for="input-term"
+        >
+          <ValidationProvider
+            name="Search term"
+            rules="required|max:64"
+            v-slot="v"
+          >
+            <b-form-input
+              id="input-term"
+              v-model="searchTerm"
+              placeholder="Enter search term"
+              required
+              :class="
+                (v.errors.length > 0 ? 'is-invalid ' : '') +
+                (v.valid ? 'is-valid' : '')
+              "
+            />
+            <b-form-invalid-feedback>
+              {{ v.errors[0] }}
+            </b-form-invalid-feedback>
+          </ValidationProvider>
+        </b-form-group>
+        <b-form-group
+          id="input-group-2"
+          label="User name"
+          label-for="input-username"
+        >
+          <ValidationProvider
+            name="User name"
+            rules="required|min:2|max:32"
+            v-slot="v"
+          >
+            <b-form-input
+              id="input-username"
+              v-model="userName"
+              placeholder="Enter your name"
+              required
+              :class="
+                (v.errors.length > 0 ? 'is-invalid ' : '') +
+                (v.valid ? 'is-valid' : '')
+              "
+            />
+            <b-form-invalid-feedback>
+              {{ v.errors[0] }}
+            </b-form-invalid-feedback>
+          </ValidationProvider>
+        </b-form-group>
+        <b-button variant="dark" block @click="start" :disabled="disableStart">Start</b-button>
+      </b-form>
+    </ValidationObserver>
+  </b-card>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Emit, Prop } from "vue-property-decorator";
+import { extend, ValidationProvider, ValidationObserver } from "vee-validate";
+import { required, min, max } from "vee-validate/dist/rules";
+
+extend("required", { ...required, message: "{_field_} is required" });
+extend("min", {
+  ...min,
+  message: "{_field_} must be at least {length} characters",
+});
+extend("max", {
+  ...max,
+  message: "{_field_} may not be greater than {length} characters",
+});
+
+@Component({
+  components: {
+    ValidationObserver,
+    ValidationProvider,
+  },
+})
+export default class StartForm extends Vue {
+  @Prop({default: false}) readonly disableStart!: boolean;
+
+  private searchTerm: string;
+  private userName: string;
+
+  constructor() {
+    super();
+    this.searchTerm = "";
+    this.userName = "";
+  }
+
+  @Emit("start-game-event")
+  startGame(searchTerm: string, userName: string): unknown {
+    return { searchTerm, userName };
+  }
+
+  async start(): Promise<void> {
+    const isValid = await this.$refs.formObserver.validate();
+    if (isValid) {
+      this.startGame(this.searchTerm, this.userName);
+    } else {
+      this.$bvToast.toast("Please, enter valid information.", {
+        title: "Invalid fields",
+        variant: "danger",
+        autoHideDelay: 4000,
+      });
+    }
+  }
+
+  $refs!: {
+    formObserver: InstanceType<typeof ValidationObserver>;
+  };
+}
+</script>
---
+++ src/views/Guessing.vue
@@ -0,0 +1,30 @@
+<template>
+  <b-row>
+    <b-col cols="12" md="4" offset-md="4">
+      <start-form :disableStart="isLoadingMovies" @start-game-event="startEvent" />
+    </b-col>
+  </b-row>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import StartForm from "@/modules/game/components/StartForm.vue";
+
+@Component({
+    components: {
+        StartForm
+    }
+})
+export default class Guessing extends Vue {  
+
+    private isLoadingMovies = false;
+
+    startEvent(form: unknown): void {
+        console.log('start!', form);
+        //this.isLoadingMovies = true;
+    }
+}
+</script>
+
+<style>
+</style>
