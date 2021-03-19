5f5b05f00af69f72fdd998ebe36bd14bdcc52c72
rodrigues@novusti.com <thiagonr86@gmail.com>
2021-03-19 08:44:23 -0300

    error handling

--- src/views/Guessing.vue
+++ src/views/Guessing.vue
@@ -1,7 +1,10 @@
 <template>
   <b-row>
     <b-col cols="12" md="4" offset-md="4">
-      <start-form :disableStart="isLoadingMovies" @start-game-event="startEvent" />
+      <start-form
+        :disableStart="isLoadingMovies"
+        @start-game-event="startEvent"
+      />
     </b-col>
   </b-row>
 </template>
@@ -13,31 +16,44 @@ import MovieService from "@/modules/game/services/MovieService";
 import Movie from "@/modules/game/models/Movie";
 
 @Component({
-    components: {
-        StartForm
-    }
+  components: {
+    StartForm,
+  },
 })
-export default class Guessing extends Vue {  
+export default class Guessing extends Vue {
+  private isLoadingMovies = false;
 
-    private isLoadingMovies = false;
+  async getMovies(searchTerm: string): Promise<Array<Movie>> {
+    const response = await new MovieService().getMoviesBySearchTerm(searchTerm);
+    return response.data;
+  }
 
-    async getMovies(searchTerm: string): Promise<Array<Movie>> {
-        const response = await new MovieService().getMoviesBySearchTerm(searchTerm);
-        return response.data;
+  handleError(response: any): void {
+    let variant = response.status == 422 ? "warning" : "danger";
+    let text =
+      response.status == 422
+        ? "Please, be more specific in your search term."
+        : response.status == 404
+        ? "No results found. Please, type other term."
+        : response.data.error;
+    this.$bvToast.toast(text, {
+      title: "Something wrong happened.",
+      variant,
+      autoHideDelay: 5000,
+    });
+  }
 
+  async startEvent(form: any): Promise<void> {
+    this.isLoadingMovies = true;
+    try {
+      const movies: Array<Movie> = await this.getMovies(form.searchTerm);
+      console.log(movies);
+    } catch (e) {
+      this.handleError(e.response);
+    } finally {
+      this.isLoadingMovies = false;
     }
-
-    async startEvent(form: any): Promise<void> {
-        this.isLoadingMovies = true;
-        try {
-            const movies: Array<Movie> =  await this.getMovies(form.searchTerm);
-            console.log(movies);
-        } catch(e) {
-            console.log(e);
-        } finally {
-            this.isLoadingMovies = false;
-        }
-    }
+  }
 }
 </script>
 
